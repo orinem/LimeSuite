c6bd887689596bb54c38504373be3e95ec2f7c79
Orin Eman <orin.eman@gmail.com>
2018-06-26 13:19:03 -0700

    Copyright notices.

--- LimeCOM/LMSDevice.cpp
+++ LimeCOM/LMSDevice.cpp
@@ -1,10 +1,27 @@
+/*
+* Copyright (C) 2018 Orin Eman
+*
+* Based on the LimeSuite API which is Copyright(C) 2016 Lime Microsystems
+* and can be found at https://github.com/myriadrf/LimeSuite
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 // LMSDevice.cpp : Implementation of CLMSDevice
 
 #include "stdafx.h"
 #include "LMSDevice.h"
 #include "LMSChannel.h"
 #include <vector>
-
 
 // CLMSDevice
 
